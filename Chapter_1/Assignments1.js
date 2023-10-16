60bb3de92aef2c914447c02f850725a7e1d81878
Sourav-Tanwar <souravtnw@gmail.com>
2023-10-16 10:46:39 +0530

    Chapter 1 - Data Types

---
+++ Chapter_1/Assignments1.js
@@ -0,0 +1,95 @@
+console.log("Hello Javascript")
+
+// Assignment 1.1
+
+let a =null;
+let b;
+
+console.log(typeof(a), typeof(b))  // ANS - object undefined 
+
+// Assignment 1.2
+
+let  c ;
+var d ;
+// const e ;   // This will show error
+
+// Assignment 1.3
+
+let languages = 'java javaScript python cSharp';
+
+let result1 = languages.lastIndexOf('S');
+
+console.log(result1); //Ans- 24
+
+// Assignment 1.4
+
+let variable = 'hello programmers';
+
+let result = Number(variable);
+
+console.log(result); // Ans - NaN (Not a Number)
+
+// Assignment 1.5
+let num1 = 32;
+
+let num2 = '32';
+
+let result3 = num1 !== num2;
+
+let result4 = num1 != num2;
+
+console.log(result3, result4); //true false
+
+// Assignment 1.6
+
+let str = 'Hello Programmers';
+
+let result5 = str.includes('r');
+
+console.log(result5); // true
+
+// Assignment 1.7
+
+let num3 = 2;
+
+let num4 = 5;
+
+let result6 = num3 ** num4 * 2;
+
+console.log(result6); // 64 
+
+
+// Assignment 1.8
+
+let num5 = [1, 2, 4, 5];
+
+let num6 = [6, 5, 8, 0];
+
+let result7 = num5.concat(num6);
+
+console.log(result7); // [1, 2, 4, 5, 6, 5, 8, 0]
+
+// Assignment 1.9
+
+let num7 = 5;
+
+let num8 = 7;
+
+let num9 = 8;
+
+let result8 = num7 < num8 > num9;
+
+console.log(result8);  // false
+
+// Assignment 1.10 
+// If your State is split into four equal parts such that in each part there are 1/4 number of people live.
+//  You have to find how many people would live in each part? which operators will you use ?  // Ans -Division
+
+
+
+
+
+
+
+
+
