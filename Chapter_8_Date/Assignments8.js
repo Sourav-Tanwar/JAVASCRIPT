5ecb895ab94c841a77bede2174e2392124e980d7
Sourav-Tanwar <souravtnw@gmail.com>
2023-10-25 13:32:06 +0530

    Chapter 8 Date

---
+++ Chapter_8_Date/Assignments8.js
@@ -0,0 +1,25 @@
+console.log("Hello Javascript")
+
+// Date
+
+
+// Assignment 8.1
+// How can we get current time in millisecond of current time?
+
+let date = new Date().getTime()
+
+console.log(date)
+
+// Assignment 8.2
+
+//  Guess the Output and Explain Why?
+
+let currentDate = new Date();
+
+let result1 = currentDate.getDay();
+
+let result2 = currentDate.getDate();
+
+console.log(result1, result2);
+
+// Output - 3 25 , Here 3 denotes wednesday
